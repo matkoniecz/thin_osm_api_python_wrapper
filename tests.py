204a54c1075406a383fcefe7cc5af8c4e4415517
Mateusz Konieczny <matkoniecz@gmail.com>
2022-03-13 15:14:29 +0100

    first useful test

--- tests.py
+++ tests.py
@@ -1,8 +1,13 @@
 import unittest
+import example_pkg_matkoniecz
 
 class Tests(unittest.TestCase):
     def test_basic_math(self):
         self.assertEqual(2-2, 0)
+
+    def test_example_code(self):
+        self.assertEqual(2-2, 0)
+        print(example_pkg_matkoniecz.lorem_ipsum.text())
 
 if __name__ == '__main__':
     unittest.main()
