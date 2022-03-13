f7ee3eaae89114a61708233675e9ea0e2ddd08b5
Mateusz Konieczny <matkoniecz@gmail.com>
2022-03-13 14:16:09 +0100

    add some helpful docs

--- setup.py
+++ setup.py
@@ -18,4 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    # for dependencies see https://python-packaging.readthedocs.io/en/latest/dependencies.html
 ) 
