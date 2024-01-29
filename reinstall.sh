94abec81a8c4a1a58d3be64b356f2d5ff9b4bdb1
Mateusz Konieczny <matkoniecz@gmail.com>
2024-01-29 15:41:46 +0100

    run install also for python3.10

--- reinstall.sh
+++ reinstall.sh
@@ -3,6 +3,8 @@ python3 setup.py sdist bdist_wheel
 cd dist
 pip3 uninstall thin_osm_api_wrapper -y
 pip3 install --user *.whl
+pip3.10 uninstall thin_osm_api_wrapper -y
+pip3.10 install --user *.whl
 cd ..
 python3 -m unittest
 # twine upload dist/* # to upload to PyPi
